ae3f8084afc8cca6d6566578466b416e4fd3f108
newac01 <newac01@students.ipfw.edu>
2017-03-26 03:08:59 -0400

    Committing Tile class and some of the GUI class

---
+++ src/com/TheGUI.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+/**
+ * Created by Aaron on 3/26/2017.
+ */
+public class TheGUI {
+    //Tiles_____
+    Tiles[][] tiles = new Tiles[9][9];
+    //Listeners_____
+    TileListener tileHandler;
+    //Panels____
+    JPanel tilePanel;
+    JPanel queuePanel;
+    JPanel buttonsPanel;
+
+    private class TileListener implements ActionListener {
+        Tiles t;
+
+        public TileListener(Tiles tile) {
+            this.t = tile;
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            t = (Tiles) e.getSource();
+        }
+    }
+}
---
+++ src/com/Tiles.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+
+/**
+ * Created by Aaron on 3/26/2017.
+ */
+public class Tiles extends JButton{
+    private int value;
+    private boolean isEmpty;
+    //constructor
+    public Tiles(int val, boolean isEmpty_){
+        this.value = val;
+        this.isEmpty = isEmpty_;
+        setText(value + "");
+
+    }
+    //used to grab integer value
+    public int getInt(){
+        return value;
+    }
+    //will be used to grab the boolean
+    public boolean getBool(){
+        return isEmpty;
+    }
+    //will be used to create numbers
+    public void setNumber(int number){
+        this.value = number;
+        setText(number + "");
+    }
+    //will be used to set the boolean
+    public void setBoolean(boolean empty){
+        this.isEmpty = empty;
+    }
+}
