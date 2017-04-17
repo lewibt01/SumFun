52ed97c5c2402c19f7d910f4a91a022ab924f0dc
Scott Babbitt <babbsa01@students.ipfw.edu>
2017-04-17 01:16:13 -0400

    fixed error in latest merge

--- src/view/CurrentScoreView.java
+++ src/view/CurrentScoreView.java
@@ -63,8 +63,8 @@ public class CurrentScoreView extends JPanel implements Observer {
     @Override
     public void update(Observable o, Object arg) {
         if (o.getClass().getSimpleName().equals("CurrentScoreModel")) {
-            scoreTextField.setText(currentScore.scoreCh(currentScore.getCurrentScore()) + "");
-            movesRemaining.setText(currentScore.move(currentScore.getNumberMoves()) + "");
+            scoreTextField.setText(currentScore.getCurrentScore() + "");
+            movesRemaining.setText(currentScore.getNumberMoves() + "");
         }
     }
 }
